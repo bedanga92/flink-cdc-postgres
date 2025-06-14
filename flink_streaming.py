5257b6c09995dd80d073002b2d77d149be65a088
Bedanga Bikash Roy Medhi <38385001+bedanga92@users.noreply.github.com>
2025-06-14 21:20:59 +0100

    Initial Commit. Sample code for flink CDC

---
+++ flink_streaming.py
@@ -0,0 +1,52 @@
+from pyflink.table import EnvironmentSettings, TableEnvironment
+import os
+
+
+# Create a batch TableEnvironment
+env_settings = EnvironmentSettings.in_streaming_mode()
+table_env = TableEnvironment.create(env_settings)
+
+# Get the current working directory
+CURRENT_DIR = os.getcwd()
+
+# Define a list of JAR file names you want to add
+jar_files = [
+    "flink-sql-connector-postgres-cdc-3.0.1.jar",
+    "postgresql-42.6.0.jar",
+    "flink-connector-jdbc-3.3.0-1.20.jar"
+]
+
+# Build the list of JAR URLs by prepending 'file:///' to each file name
+jar_urls = [f"file:///{CURRENT_DIR}/{jar_file}" for jar_file in jar_files]
+
+table_env.get_config().get_configuration().set_string(
+    "pipeline.jars",
+    ";".join(jar_urls)
+)
+
+postgres_sink = f"""
+CREATE TABLE shipments (
+   shipment_id INT,
+   order_id INT,
+   origin STRING,
+   destination STRING,
+   is_arrived BOOLEAN,
+   PRIMARY KEY (shipment_id) NOT ENFORCED
+ ) WITH (
+   'connector' = 'postgres-cdc',
+   'hostname' = 'localhost',
+   'port' = '5432',
+   'username' = 'postgres',
+   'password' = 'postgres',
+   'database-name' = 'postgres',
+   'schema-name' = 'public',
+    'slot.name' = 'sales',
+    'decoding.plugin.name' = 'pgoutput',
+   'table-name' = 'shipments'
+ );
+"""
+
+table_env.execute_sql(postgres_sink)
+
+
+table_env.execute_sql(f"SELECT * FROM shipments ").print()
---
+++ init-jars-project.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+curl -o flink-sql-connector-postgres-cdc-3.0.1.jar https://repo1.maven.org/maven2/com/ververica/flink-sql-connector-postgres-cdc/3.0.1/flink-sql-connector-postgres-cdc-3.0.1.jar
+curl -o postgresql-42.6.0.jar https://repo1.maven.org/maven2/org/postgresql/postgresql/42.6.0/postgresql-42.6.0.jar
+curl -o flink-connector-jdbc-3.3.0-1.20.jar https://repo1.maven.org/maven2/org/apache/flink/flink-connector-jdbc/3.3.0-1.20/flink-connector-jdbc-3.3.0-1.20.jar
